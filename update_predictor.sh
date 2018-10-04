3922fd836de77ae7a827d72c7d5f8cc348697c45
Siceth <seth@seth-phillips.com>
2018-10-04 19:05:40 -0400

    Reset naming scheme

--- update_predictor.sh
+++ update_predictor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget -q -N https://raw.githubusercontent.com/Siceth/Stratus-Stat-Utilities/master/Stratus%20Stat%20Utilities.py
+mv "Stratus Stat Utilities.py" "predict.py"
 sed -i 's/HEADLESS_MODE = False/HEADLESS_MODE = True/g' "Stratus Stat Utilities.py"
 sed -i 's/REALTIME_MODE = False/REALTIME_MODE = True/g' "Stratus Stat Utilities.py"
