b8e9ceb80e304c417a2319dff6ae1cdbbd8b2b69
Emilien Devos <contact@emiliendevos.be>
2018-11-04 01:41:40 +0100

    enforce regex for chat

--- bot.js
+++ bot.js
@@ -188,9 +188,9 @@ setInterval(function () {
 }, 5000);
 
 function connect(bot, teams) {
-    bot.chatAddPattern(/<(?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+)>: (.*)$/, 'chat', 'chat global');
-    bot.chatAddPattern(/\(Team\) (?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+): (.*)$/, 'chat', 'chat team');
-    bot.chatAddPattern(/\[PM\] From (?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+): (.*)$/, 'whisper', 'Private Message');
+    bot.chatAddPattern(/^<(?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+)>: (.*)$/, 'chat', 'chat global');
+    bot.chatAddPattern(/^\(Team\) (?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+): (.*)$/, 'chat', 'chat team');
+    bot.chatAddPattern(/^\[PM\] From (?:\[[\w]+\] |[\W]|[\W]\[[\w]+\])?([\w\d_]+): (.*)$/, 'whisper', 'Private Message');
     bot.chatAddPattern(/Current Rotation \(([a-zA-Z]+)\)/, 'rotcmd', 'Rotation');
     bot.chatAddPattern(/^The time limit is (.+) with the result(?:.*?)/, 'tlcmd', 'Time limit');
     bot.chatAddPattern(/^Next map: ([\w\d' :]+) by/, 'nextmapcmd', 'Next map');
